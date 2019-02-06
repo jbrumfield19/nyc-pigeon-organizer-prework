f1bdc5808e1a713c7ad2c5c3b291cd4efb3700be
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-02-06 14:30:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,12 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list ={}
-  data.each do |(key, value), hash|
-    hash.each do 
+  data.each_with_object({}) do |(key, h), result|
+ 	    h.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
   end
+end
 end
